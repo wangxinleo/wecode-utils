cd21e988957fff0be1ef8f1626120d89669aa267
wangxinleo <wangxin.leo@outlook.com>
2021-05-05 22:47:42 +0800

    date-utils.js

--- src/date-utils.js
+++ src/date-utils.js
@@ -14,4 +14,12 @@ module.exports = {
       + pad(date.getUTCMinutes()) + ":"
       + pad(date.getUTCSeconds()) + "Z";
   },
+
+  /**
+   * 返回两个日期之间的差异 (以天为值)
+   * @param dateInitial
+   * @param dateFinal
+   * @returns {number}
+   */
+  getDayDiffBetweenDates: (dateInitial, dateFinal) => (dateFinal - dateInitial) / (1000 * 3600 * 24),
 }
