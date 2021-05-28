1725f99b993ab4208957d55e080321aca39e0edd
wangxinleo <wangxin.leo@outlook.com>
2021-05-28 14:30:46 +0800

    update 1430

--- test/index.js
+++ test/index.js
@@ -1,18 +1,12 @@
 const js_utils = require('../index');
 
-// console.log(js_utils.arrayChunk([1,2,3,4,5],2))
-//
-// console.log(js_utils.arrayFilterFalse([0,false,null,undefined,"",1]))
-//
-// console.log(js_utils.arrayCount([1,1,1,1,2],1))
-//
-// console.log(js_utils.arrayDisassemble([[1,3,4,5],[2,3,[4,4,5]],1,1,2]))
-//
-// console.log(js_utils.arrayZip([1,1,2,2,0,3,3],[1],[2]))
+console.log(js_utils.arrayChunk([1, 2, 3, 4, 5], 2));
 
-const delay = (d, callback) => {
-  return setTimeout(callback, d)
-}
-delay(2000,(err, result) =>
-  err ? console.log(err) : console.log(result)
-)
+console.log(js_utils.arrayFilterFalse([0, false, null, undefined, "", 1]));
+
+console.log(js_utils.arrayCount([1, 1, 1, 1, 2], 1));
+
+console.log(js_utils.arrayDisassemble([[1, 3, 4, 5], [2, 3, [4, 4, 5]], 1, 1, 2]));
+
+console.log(js_utils.arrayZip([1, 1, 2, 2, 0, 3, 3], [1], [2]));
+
