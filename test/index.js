406977bcc930090130309f7094fed60d422b83e6
wangxinleo <wangxin.leo@outlook.com>
2021-05-29 10:48:24 +0800

    update array doc

--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
 const js_utils = require('../index');
 
-console.log(js_utils.arrayChunk([1, 2, 3, 4, 5], 2));
+// console.log(js_utils.arrayChunk([1, 2, 3, 4, 5], 2));
 
-console.log(js_utils.arrayFilterFalse([0, false, null, undefined, "", 1]));
+// console.log(js_utils.arrayFilterFalse([0, false, null, undefined, "", 1]));
 
-console.log(js_utils.arrayCount([1, 1, 1, 1, 2], 1));
+// console.log(js_utils.arrayCount([1, 1, 1, 1, 2], 1));
 
-console.log(js_utils.arrayDisassemble([[1, 3, 4, 5], [2, 3, [4, 4, 5]], 1, 1, 2]));
+// console.log(js_utils.arrayDisassemble([[1, 3, 4, 5], [2, 3, [4, 4, 5]], 1, 1, 2]));
 
-console.log(js_utils.arrayZip([1, 1, 2, 2, 0, 3, 3], [1], [2]));
+// console.log(js_utils.arrayZip([1, 1, 2, 2, 0, 3, 3], [1], [2]));
 
+console.log(js_utils.arrayOnlyUnique([10, 10, 2, 2, 1]));
+console.log(js_utils.arrayDifference([10, 1, 2], [10, 2, 3]));
+console.log(js_utils.arrayZip([1, 2], [3, 4]));
